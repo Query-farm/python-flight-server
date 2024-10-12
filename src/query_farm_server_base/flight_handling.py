4c0390531e4bdf3869b47c22ebf0fa6f46429312
Rusty Conover <rusty@conover.me>
2024-10-11 20:41:32 -0400

    add parse_filter_info

--- src/query_farm_server_base/flight_handling.py
+++ src/query_farm_server_base/flight_handling.py
@@ -1,9 +1,13 @@
 import io
 import json
 import struct
+from typing import Any, TypedDict
 
 import pyarrow.flight as flight
+import sqlglot
+import structlog
 import zstandard as zstd
+from duckdb_query_tools import duckdb_serialized_expression, sql_statement_analyzer
 
 ticket_with_metadata_indicator = b"<TICKET_WITH_METADATA>"
 
@@ -42,3 +46,60 @@ def decode_ticket_with_metadata(ticket: flight.Ticket) -> tuple[str, dict[str, l
     else:
         decoded_ticket = ticket.ticket.decode("utf-8")
         return decoded_ticket, {}
+
+
+class ParsedFilterInfo(TypedDict):
+    parsed_parameter_values: dict[str, list[Any]]
+    filter_sql_where_clause: str | None
+    filter_types_as_sql: dict[str, str]
+
+
+log = structlog.get_logger()
+
+
+def parse_filter_info(
+    *, filter_data: Any, input_field_names: list[str], fields_where_values_are_known: set[str]
+) -> ParsedFilterInfo:
+    filter_sql_where_clause: str | None = None
+
+    if all(key in filter_data for key in ("filters", "column_binding_names_by_index")):
+        filter_sql_where_clause, filter_sql_field_type_info = duckdb_serialized_expression.convert_to_sql(
+            source=filter_data["filters"],
+            bound_column_names=filter_data["column_binding_names_by_index"],
+        )
+
+    # There may be additional columns specified in the filter, but not actually necessary to apply to the API call
+    # so those should be removed.
+    if filter_sql_where_clause == "" or filter_sql_where_clause is None:
+        parsed_parameter_values = {}
+        filter_types_as_sql = {}
+    else:
+        ft = sql_statement_analyzer._filter_column_references_statement(
+            f"select * from data where {filter_sql_where_clause}", input_field_names
+        )
+
+        filter_where_clause = ft.find(sqlglot.exp.Where)
+        if filter_where_clause is not None:
+            filter_sql_where_clause = filter_where_clause.sql(dialect="duckdb")[len("WHERE ") :]
+        else:
+            filter_sql_where_clause = None
+
+        log.debug("Filtered where clause", filter_sql=filter_sql_where_clause)
+        # Now that we have the filter sql, extract all of the values for the input parameters
+        # of this boto3 call.
+
+        parsed_parameter_values = sql_statement_analyzer.determine_input_column_values(
+            f"select * from data where {filter_sql_where_clause}",
+            input_field_names,
+            fields_where_values_are_known,
+        )
+
+        log.debug("Parsed parameters", values=parsed_parameter_values)
+
+        filter_types_as_sql = duckdb_serialized_expression.convert_type_to_sql(filter_sql_field_type_info)
+
+    return ParsedFilterInfo(
+        parsed_parameter_values=parsed_parameter_values,
+        filter_sql_where_clause=filter_sql_where_clause,
+        filter_types_as_sql=filter_types_as_sql,
+    )
