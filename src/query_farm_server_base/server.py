c6caf4cd4ea1dfcaf173d3d754c5e7371b9e8895
Rusty Conover <rusty@conover.me>
2025-01-18 17:32:09 -0500

    fixes

--- src/query_farm_server_base/server.py
+++ src/query_farm_server_base/server.py
@@ -165,7 +165,6 @@ class BasicFlightServer(flight.FlightServerBase, Generic[AuthManager]):
         )
 
         logger.info("do_exchange")
-        breakpoint()
 
         return self.impl_do_exchange(
             context=context,
