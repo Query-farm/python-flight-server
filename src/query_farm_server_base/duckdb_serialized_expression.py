c9c0a6358b998f43288306fa95d79606232e40f3
Rusty Conover <rusty@conover.me>
2025-06-22 22:36:17 -0400

    fixes

--- src/query_farm_server_base/duckdb_serialized_expression.py
+++ src/query_farm_server_base/duckdb_serialized_expression.py
@@ -141,6 +141,8 @@ def expression_to_string(
             return "INTERVAL '" + f"{iv['months']} months {iv['days']} days {iv['micros']} us" + "'"
         elif expression["value"]["type"]["id"] == "TIMESTAMP":
             return f"make_timestamp({expression['value']['value']}::bigint)"
+        elif expression["value"]["type"]["id"] == "TIME":
+            return f"TIME '{expression['value']['value']}'"
         elif expression["value"]["type"]["id"] == "TIMESTAMP_S":
             return f"make_timestamp({expression['value']['value']}::bigint*1000000)"
         elif expression["value"]["type"]["id"] == "TIMESTAMP WITH TIME ZONE":
